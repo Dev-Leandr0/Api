9b29d607389961793fcce73d9d8a16c8ed7431b3
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-10-09 16:58:03 -0300

    style: mejorar legibilidad en authorizeMiddleware.js

--- src/middleware/authorizeMiddleware.js
+++ src/middleware/authorizeMiddleware.js
@@ -3,10 +3,8 @@ const authorizationAdmin = (req, res, next) => {
   const user = req.user;
   if (!user || user.role !== "admin") {
     return res.status(404).send('Acceso Denegados');
-  }
+  };
   next();
-}
+};
 
-
-
-module.exports = authorizationAdmin
+module.exports = authorizationAdmin;
