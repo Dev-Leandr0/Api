09d3627a2a923d995b0de68bfa5114d0311c4623
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-10-09 16:58:12 -0300

    style: mejorar legibilidad en verifyToken.js

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -1,16 +1,16 @@
 var jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-
   const token = req.headers.authorization;
+
   if (!token) {
     return res.status(400).send('Token requerido');
-  }
+  };
 
   jwt.verify(token.split(' ')[1], 'MySecretKey', (err, decode) => {
     if (err) {
-      return res.send(400).send('Token invalido');
-    }
+      return res.status(400).send('Token inválido');
+    };
     req.user = decode;
     next();
   });
