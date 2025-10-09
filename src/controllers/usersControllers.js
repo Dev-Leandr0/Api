4dd0f686476f8bb197a92337bda4650fc8f28053
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-10-09 16:57:09 -0300

    style: mejorar legibilidad en usersControllers.js

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,48 +1,48 @@
-const users = require('../db/database');
+const User = require('../models/User.js');
 const bcrypt = require('bcrypt');
+// const { required } = require('joi');
 
-const createUserController = async (name, username, email, password, role) => {
-  const id = users.length + 1;
+const createUserController = async (name, username, email, password, phone, role) => {
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = { id, name, username, email, password: hashPassword, role };
-  if (!name || !username || !email || !password) throw new Error(`Los datos están incompletos`);
-  users.push(newUser);
+
+  const newUser = new User({ name, username, email, password: hashPassword, phone, role });
+
+  if (!name || !username || !email || !password || !phone) throw new Error(`Los datos están incompletos`);
+
+  await newUser.save();
   return newUser;
+
 };
 
-const getAllUsersController = () => {
-  return users;
-}
+const getAllUsersController = async () => {
+  if (!User.length) throw new Error("No hay Usuarios");
+  return await User.find();
+};
 
-const getUsersByNameController = (name) => {
-  const userByName = users.filter((user) => user.name === name);
-  if (!userByName.length) throw new Error(`No se encontró al usuario`);
+const getUsersByNameController = async (name) => {
+  const userByName = await User.find({ name });
+  if (!userByName.length)
+    throw new Error(`No se encontró al usuario`);
   return userByName;
-}
+};
 
-const getOneUserByIdController = (id) => {
-  const userById = users.find((user) => user.id === Number(id));
-  if (!userById) throw new Error(`No se encontró el usuario con ese ID`);
+const getOneUserByIdController = async (id) => {
+  const userById = await User.findById(id);
+  if (!userById)
+    throw new Error(`No se encontró el usuario con ese ID`);
   return userById;
-}
+};
 
-const updateUserController = (id, name, username, email) => {
-  const newUser = { name, username, email };
-  const userById = users.find((user) => user.id === Number(id));
-  if (userById) {
-    Object.assign(userById, newUser);
-  }
-  return userById;
-}
+const updateUserController = async (id, name, username, email, phone) => {
+  const newUser = { name, username, email, phone };
+  const updateUser = await User.findByIdAndUpdate(id, newUser, { new: true });
+  return updateUser;
+};
 
-const deleteUserController = (id) => {
-  const index = users.findIndex((user) => user.id === Number(id));
-  let deleteUser;
-  if (index !== -1) {
-    [deleteUser] = users.splice(index, 1);
-  }
+const deleteUserController = async (id) => {
+  const deleteUser = await User.findByIdAndDelete(id);
   return deleteUser;
-}
+};
 
 module.exports = {
   createUserController,
