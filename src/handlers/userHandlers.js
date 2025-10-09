2a1ea198179a2c668c874e358bbf428fe479c065
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-10-09 16:57:52 -0300

    style: mejorar legibilidad en userHandlers.js

--- src/handlers/userHandlers.js
+++ src/handlers/userHandlers.js
@@ -1,7 +1,6 @@
 const { createUserController, getAllUsersController, getUsersByNameController, getOneUserByIdController, updateUserController, deleteUserController } = require("../controllers/usersControllers");
 
 const Joi = require('joi');
-
 const userSchema = Joi.object({
   name: Joi.string()
     .min(3)
@@ -19,7 +18,12 @@ const userSchema = Joi.object({
     .pattern(new RegExp(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]).{8,}$/))
     .required(),
   role: Joi.string()
-    .valid('user', 'admin')
+    .valid('user', 'admin'),
+  phone: Joi.string()
+    .pattern(/^[0-9]{7,15}$/)
+    .optional(),
+  isActive: Joi.boolean()
+    .default(true)
 });
 
 const createUserHandler = async (req, res) => {
@@ -28,53 +32,59 @@ const createUserHandler = async (req, res) => {
     const { error } = userSchema.validate(req.body);
     if (error) res.status(404).send(error.details[0].message);
 
-    const { name, username, email, password, role } = req.body;
-    const response = await createUserController(name, username, email, password, role);
+    const { name, username, email, password, phone, role } = req.body;
+    const response = await createUserController(name, username, email, password, phone, role);
+
     res.status(201).send(response);
+
   } catch (error) {
     res.status(404).send({ Error: error.message });
-  }
-}
+  };
+};
 
-const getAllUserHandler = (req, res) => {
+const getAllUserHandler = async (req, res) => {
   try {
+
     const { name } = req.query;
     if (name) {
-      const response = getUsersByNameController(name);
+      const response = await getUsersByNameController(name);
       res.status(200).send(response);
+
     } else {
-      const response = getAllUsersController();
+      const response = await getAllUsersController();
       res.status(200).send(response);
-    }
+    };
+
   } catch (error) {
     res.status(404).send({ Error: error.message });
-  }
+  };
+};
 
-}
-
-const getOneUserHandler = (req, res) => {
+const getOneUserHandler = async (req, res) => {
   try {
     const { id } = req.params;
-    const response = getOneUserByIdController(id);
+    const response = await getOneUserByIdController(id);
     res.status(200).send(response);
+
   } catch (error) {
     res.status(404).send({ Error: error.message });
-  }
+  };
+};
 
-}
-
-const updateUserHandler = (req, res) => {
+const updateUserHandler = async (req, res) => {
   const { id } = req.params;
-  const { name, username, email } = req.body;
-  const response = updateUserController(id, name, username, email);
-  res.status(200).send(response);
-}
+  const { name, username, email, phone } = req.body;
+  const response = await updateUserController(id, name, username, email, phone);
 
-const deleteUserHandler = (req, res) => {
-  const { id } = req.params;
-  const response = deleteUserController(id);
   res.status(200).send(response);
-}
+};
+
+const deleteUserHandler = async (req, res) => {
+  const { id } = req.params;
+  const response = await deleteUserController(id);
+
+  res.status(200).send(response);
+};
 
 module.exports = {
   getAllUserHandler,
