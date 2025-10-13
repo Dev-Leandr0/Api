478338efaca94805ff5f4d20e10478e9885762f5
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-10-13 09:30:37 -0300

    chore: configurar middleware de logging con Morgan en app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const morgan = require('morgan');
 const mainRoute = require('./Routes/mainRoute');
+const setupLogger = require('./config/logger');
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
+
+// Configuración de Morgan
+setupLogger(app);
+
+// Ruta principal
 app.use('/api', mainRoute);
 
 module.exports = app;
