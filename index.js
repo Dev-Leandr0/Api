df6037b57efe3f6bfe3de5b14d355f9fee7378e8
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-10-16 13:17:06 -0300

    chore: mejorar logs de inicio y carga de modelos; agregar sequelize y relaciones

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 require('dotenv').config({ quiet: true });
 const app = require('./src/app');
-const mongoose = require('./src/db/database');
-
+const { sequelize } = require('./src/db/database');
+const { User, Product, Cart } = require('./src/models/relations');
+console.log('- Modelos cargados:', Object.keys({ User, Product, Cart }));
 
 const port = process.env.PORT || 3001;
 
-mongoose.connection.once('open', () => {
-  console.log('DB conectada correctamente');
-  app.listen(port, () => {
-    console.log(`Servidor escuchando en el puerto ${port}`);
-  });
-});
+async function main() {
+  try {
+    await sequelize.authenticate();
+    console.log('- DB conectada correctamente');
 
-mongoose.connection.on('error', (err) => {
-  console.error('Error de conexión a MongoDB:', err);
-});
+    await sequelize.sync({ alter: true }); // force: true
+    console.log('- Tablas sincronizadas correctamente\n');
+
+    console.log(`- Servidor corriendo en modo: ${process.env.NODE_ENV}`);
+
+    app.listen(port, () => {
+      console.log(`- Servidor escuchando en el puerto ${port}`);
+    });
+
+  } catch (error) {
+
+    console.error('Error de conexión a PostgreSQL:', error);
+    process.exit(1);
+  };
+};
+
+main();
