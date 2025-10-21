e271960a75119099fb7c316161d0ef1af2c26d5f
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-10-21 17:10:07 -0300

    feat: agregar rutas de categorías al enrutador principal

--- src/Routes/mainRoute.js
+++ src/Routes/mainRoute.js
@@ -4,6 +4,7 @@ const mainRoute = Router();
 const userRoutes = require('./userRoutes');
 const productRoutes = require('./productRoutes');
 const authRoutes = require('./authRoutes');
+const categoryRoutes = require('./categoryRoutes');
 
 // Users
 mainRoute.use('/users', userRoutes);
@@ -13,5 +14,8 @@ mainRoute.use('/products', productRoutes);
 
 //Auth 
 mainRoute.use('/auth', authRoutes);
+
+// Category
+mainRoute.use('/categories', categoryRoutes);
 
 module.exports = mainRoute;
