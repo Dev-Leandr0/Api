570faba47ae727306e46a503e5db38b75dae13e4
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-10-21 09:12:04 -0300

    feat: establecer relaciones entre Category y Product en Sequelize

--- src/models/relations.js
+++ src/models/relations.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Product = require('./Product');
 const Cart = require('./Cart');
+const Category = require('./Category');
 
 // Relaciones User - Cart
 User.hasMany(Cart, { foreignKey: 'userId' });
@@ -10,8 +11,14 @@ Cart.belongsTo(User, { foreignKey: 'userId' });
 Product.hasMany(Cart, { foreignKey: 'productId' });
 Cart.belongsTo(Product, { foreignKey: 'productId' });
 
+// Relaciones Categoria - Product
+Category.hasMany(Product, { foreignKey: 'categoryId', as: 'products' });
+Product.belongsTo(Category, { foreignKey: 'categoryId', as: 'category' });
+
+
 module.exports = {
   User,
   Product,
-  Cart
+  Cart,
+  Category,
 }
