62f1453bc7ebf6a89bdaf26ab9c11ffa84a853e4
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-10-25 12:12:43 -0300

    feat(routes): agregar rutas de usuario /email, /role y /:id/status

--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { createUserHandler, getAllUsersHandler, getUserByIdHandler, getUsersByNameHandler, getUsersByStatusHandler, updateUserHandler, deleteUserHandler, deleteSoftUserHandler } = require("../handlers/userHandlers");
+const { createUserHandler, getAllUsersHandler, getUserByIdHandler, getUsersByNameHandler, getUsersByEmailHandler, getUsersByStatusHandler, getUsersByRoleHandler, updateUserHandler, updateUserStatusHandler, deleteUserHandler, deleteSoftUserHandler } = require("../handlers/userHandlers");
 
 const verifyToken = require('../middleware/verifyToken');
 const authorizationAdmin = require('../middleware/authorizeMiddleware');
@@ -12,6 +12,8 @@ userRoutes.post('/', createUserHandler);
 /* ===== Rutas Fijas ===== */
 userRoutes.get('/status', getUsersByStatusHandler);
 userRoutes.get('/name', getUsersByNameHandler);
+userRoutes.get('/email', getUsersByEmailHandler);
+userRoutes.get('/role', getUsersByRoleHandler);
 userRoutes.get('/token', verifyToken, authorizationAdmin, getAllUsersHandler);
 
 /* ===== Rutas Generales ===== */
@@ -19,8 +21,8 @@ userRoutes.get('/', getAllUsersHandler);
 userRoutes.get('/:id', getUserByIdHandler);
 
 /* ===== Update ===== */
+userRoutes.patch('/:id/status', updateUserStatusHandler);
 userRoutes.put('/:id', updateUserHandler);
-// userRoutes.patch('/:id', updateUserHandler);
 
 /* ===== Delete ===== */
 userRoutes.delete('/soft/:id', deleteSoftUserHandler);
